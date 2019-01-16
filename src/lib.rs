888be4d96a3a5d9e56ea5cf0c43f173d44b2a923
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2019-01-16 10:11:31 -0500

    debug info

--- src/lib.rs
+++ src/lib.rs
@@ -1057,6 +1057,7 @@ impl<'a> System<'a> for ForceUprightSystem {
         (&mut transforms, &uprights).join().map(|t| t.0).for_each(|tr| {
             // roll, pitch, yaw
             let angles = tr.rotation().euler_angles();
+            info!("Force upright angles: {:?}",angles);
             let new_quat = UnitQuaternion::from_euler_angles(0.0, angles.1, 0.0);
             *tr.rotation_mut() = new_quat;
         });
